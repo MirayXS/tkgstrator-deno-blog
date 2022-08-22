9438d512eacfdbd4c56509ade8615cdca4fd4c1a
tkgstrator <nasawake.am@gmail.com>
2022-08-22 17:43:43 +0900

    - 記事の修正

--- main.ts
+++ main.ts
@@ -1,4 +1,8 @@
-import blog, { ga, redirects } from "https://deno.land/x/blog@0.4.2/blog.tsx";
+import blog, {
+  ga,
+  redirects,
+  h,
+} from "https://deno.land/x/blog@0.4.2/blog.tsx";
 
 blog({
   title: "えむいーと愉快な仲間たち",
@@ -6,22 +10,12 @@ blog({
   avatar: "assets/03.png",
   avatarClass: "rounded-full",
   author: "tkgstrator",
+  // dateStyle: "long",
+  lang: "ja",
   links: [
     { title: "Email", url: "mailto:nasawake.am@gmail.com" },
     { title: "GitHub", url: "https://github.com/tkgstrator" },
     { title: "Twitter", url: "https://twitter.com/tkgling" },
   ],
-
-  // middlewares: [
-
-  // If you want to set up Google Analytics, paste your GA key here.
-  // ga("UA-XXXXXXXX-X"),
-
-  // If you want to provide some redirections, you can specify them here,
-  // pathname specified in a key will redirect to pathname in the value.
-  // redirects({
-  //  "/hello_world.html": "/hello_world",
-  // }),
-
-  // ]
+  // middlewares: [ga("UA-111335437-2"), redirects({})],
 });

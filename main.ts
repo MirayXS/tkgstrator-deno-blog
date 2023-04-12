93088754f8de8af030bd123e424027d0e1a43bb1
tkgstrator <nasawake.am@gmail.com>
2023-04-12 10:03:40 +0900

    - 記事の追加

--- main.ts
+++ main.ts
@@ -2,33 +2,34 @@ import blog, {
   ga,
   redirects,
   h,
-} from "https://raw.githubusercontent.com/tkgstrator/deno_blog/main/blog.tsx";
-import "https://esm.sh/prismjs@1.28.0/components/prism-swift.min?no-check";
-import "https://esm.sh/prismjs@1.28.0/components/prism-typescript.min?no-check";
-import "https://esm.sh/prismjs@1.28.0/components/prism-python.min?no-check";
-import "https://esm.sh/prismjs@1.28.0/components/prism-bash.min?no-check";
-import "https://esm.sh/prismjs@1.28.0/components/prism-json.min?no-check";
-import "https://esm.sh/prismjs@1.28.0/components/prism-scheme.min?no-check";
+} from 'https://raw.githubusercontent.com/tkgstrator/deno_blog/main/blog.tsx';
+import 'https://esm.sh/prismjs@1.28.0/components/prism-swift.min?no-check';
+import 'https://esm.sh/prismjs@1.28.0/components/prism-typescript.min?no-check';
+import 'https://esm.sh/prismjs@1.28.0/components/prism-python.min?no-check';
+import 'https://esm.sh/prismjs@1.28.0/components/prism-bash.min?no-check';
+import 'https://esm.sh/prismjs@1.28.0/components/prism-json.min?no-check';
+import 'https://esm.sh/prismjs@1.28.0/components/prism-scheme.min?no-check';
+import 'https://esm.sh/prismjs@1.28.0/components/prism-yaml.min?no-check';
 
 blog({
-  title: "ぼっち・ざ・えむいー",
-  description: "スターライトアーニャです",
-  avatar: "/assets/ico_ep2_01.png",
-  avatarClass: "rounded-full",
-  author: "tkgstrator",
+  title: 'ぼっち・ざ・えむいー',
+  description: 'スターライトアーニャです',
+  avatar: '/assets/ico_ep2_01.png',
+  avatarClass: 'rounded-full',
+  author: 'tkgstrator',
   // cover: "https://tkgstrator.deno.dev/assets/header.jpeg",
   // dateStyle: "long",
-  lang: "ja",
+  lang: 'ja',
   links: [
-    { title: "Email", url: "mailto:nasawake.am@gmail.com" },
-    { title: "GitHub", url: "https://github.com/tkgstrator" },
-    { title: "Twitter", url: "https://twitter.com/tkgling" },
+    { title: 'Email', url: 'mailto:nasawake.am@gmail.com' },
+    { title: 'GitHub', url: 'https://github.com/tkgstrator' },
+    { title: 'Twitter', url: 'https://twitter.com/tkgling' },
   ],
   ogImage: {
-    url: "https://tkgstrator.deno.dev/assets/header.jpeg",
-    twitterCard: "summary",
+    url: 'https://tkgstrator.deno.dev/assets/header.jpeg',
+    twitterCard: 'summary',
   },
-  favicon: "/assets/favicon.ico",
+  favicon: '/assets/favicon.ico',
   entries: 10,
-  middlewares: [ga("UA-111335437-2"), redirects({})],
+  middlewares: [ga('UA-111335437-2'), redirects({})],
 });
